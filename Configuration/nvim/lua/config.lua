38657583031ed3311014a3eeece1d107a538acbb
P217F <vuongiavinhdieu2k9@gmail.com>
2025-08-08 17:11:33 +0700

    #11 Commit: add new something into Configuration

---
+++ Configuration/nvim/lua/config.lua
@@ -0,0 +1,54 @@
+-- Fast reload
+vim.opt.compatible = false
+
+-- Encoding
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+-- Indent
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Display
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+
+-- timeout
+vim.opt.timeoutlen = 180
+vim.opt.updatetime = 15
+
+-- UI
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+-- Leader
+vim.g.mapleader = " "
+
+-- Keymap
+vim.keymap.set('n', 'w', ':w<CR>')
+vim.keymap.set('n', 'q', ':q<CR>')
+vim.keymap.set('n', 'qq',':q!<CR>')
+vim.keymap.set('n', 'wqq', ':wq!<CR>')
+
+-- Others
+vim.opt.fillchars = { eob=" " }
+vim.opt.shortmess:append("sI")
+vim.opt.whichwrap:append("<,>,[,],h,l")
+
+-- Require
+require("colorscheme")
+require("pair")
+require("folder_tree")
+require("lsp_config")
+require("nvim_lualine")
---
+++ Configuration/nvim/lua/lsp_config.lua
@@ -0,0 +1,13 @@
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+
+require('lspconfig').clangd.setup({
+  cmd = { "clangd" }, filetypes = { "c", "cpp", "h" }
+})
+
