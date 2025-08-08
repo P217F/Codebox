4491fc2d31771deaafcdf7c577e85dd523bb7ddf
P217F <vuongiavinhdieu2k9@gmail.com>
2025-08-08 22:31:51 +0700

    #12 Commit: add new something into Configuration/nvim

---
+++ Configuration/nvim/lua/telescope_conf.lua
@@ -0,0 +1,15 @@
+require('telescope').setup({
+  defaults = {
+    layout_strategy = 'horizontal',
+    sorting_strategy = 'ascending',
+    layout_config = {
+      prompt_position = 'top',
+    },
+  },
+})
+
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, {})
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, {})
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, {})
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, {})
+
